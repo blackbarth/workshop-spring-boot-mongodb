c321d2e2cbf530ba2eae5bfb11db8ba792af3060
blackbarth <blackbarth@outlook.com>
2019-03-28 10:42:00 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/luisaugustoferreira/services/UserService.java
+++ src/main/java/br/com/luisaugustoferreira/services/UserService.java
@@ -1,21 +1,27 @@
 package br.com.luisaugustoferreira.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.luisaugustoferreira.domain.User;
 import br.com.luisaugustoferreira.repository.UserRepository;
+import br.com.luisaugustoferreira.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 }
